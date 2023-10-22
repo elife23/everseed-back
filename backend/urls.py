3b8aff786b453b1dbeb870660e4b0008ab028cba
maxence <maxrychel100@gmail.com>
2023-10-22 08:11:08 +0000

    Add AddCommentWhiteboard/ API

--- backend/urls.py
+++ backend/urls.py
@@ -14,8 +14,9 @@ urlpatterns = [
     path('LaunchMeeting/', views.LaunchMeeting), # Lancer un meeting
     path('SettingMeeting/<int:roomName>/', views.SettingMeeting), # Paramètrer une salle de réunion
     path('JoinMeeting/<int:roomName>/', views.JoinMeeting), # Rejoindre un meeting
-    path('AddCommentMeeting/<int:roomName>/Participant/<int:pkUser>/', views.AddCommentMeeting), # Ajouter un commentaire dans le meeting
-    path('ViewCommentMeeting/<int:roomName>/', views.ViewCommentMeeting), # Voir tous les commentaires d'un meeting
+    path('AddCommentMeeting/<int:roomName>/Participant/<int:pkUser>/', views.AddCommentMeeting), # Ajouter un comment dans le meeting
+    path('ViewCommentMeeting/<int:roomName>/', views.ViewCommentMeeting), # Voir tous les comments d'un meeting
     path('LaunchWhiteboard/', views.LaunchWhiteboard), # Lancer un whiteboard
-    path('ViewCommentWhiteboard/<int:whiteName>/', views.ViewCommentWhiteboard), # Lancer un whiteboard
+    path('AddCommentWhiteboard/<int:whiteName>/', views.AddCommentWhiteboard), # Ajouter un comment dans le whiteboard
+    path('ViewCommentWhiteboard/<int:whiteName>/', views.ViewCommentWhiteboard), # Voir tous les comments d'un whiteboard
 ]
