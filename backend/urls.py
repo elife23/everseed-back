c2e3ab0bcbbbb668eb3b88c60ce4b294c43b36ea
maxence <maxrychel100@gmail.com>
2023-10-22 06:21:13 +0000

    Add LaunchWhiteboard/ API and update other APIs path

--- backend/urls.py
+++ backend/urls.py
@@ -11,9 +11,10 @@ urlpatterns = [
     path('SignIn/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'), # Authentifier un user avec génération d'un token de connexion
     path('SignIn/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # Mettre à jour le token de connexion
     path('ViewUser/<int:pkUser>/', views.ViewUser), # Afficher les informations d'utilisateur
-    path('StartMeeting/', views.StartMeeting), # générer le code d'un meeting
+    path('LaunchMeeting/', views.LaunchMeeting), # Lancer un meeting
     path('SettingMeeting/<int:roomName>/', views.SettingMeeting), # Paramètrer une salle de réunion
     path('JoinMeeting/<int:roomName>/', views.JoinMeeting), # Rejoindre un meeting
     path('AddCommentMeeting/<int:roomName>/Participant/<int:pkUser>/', views.AddCommentMeeting), # Ajouter un commentaire dans le meeting
     path('ViewCommentMeeting/<int:roomName>/', views.ViewCommentMeeting), # Voir tous les commentaires d'un meeting
+    path('LaunchWhiteboard/', views.LaunchWhiteboard), # Lancer un whiteboard
 ]
