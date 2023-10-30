72cbcc775cb399361c31dede82e8d065aa34f712
maxence <maxrychel100@gmail.com>
2023-10-30 05:19:14 +0000

    Remove SignIn/refresh route of SignIn/ API

--- backend/urls.py
+++ backend/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
 from . import views 
 from .views import MyTokenObtainPairView  
-from rest_framework_simplejwt.views import (
-    #TokenObtainPairView,
-    TokenRefreshView,
-)
+
 
 urlpatterns = [
     path('SignUp/', views.SignUp), # Enregistrer un user
     path('SignIn/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'), # Authentifier un user avec génération d'un token de connexion
-    path('SignIn/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # Mettre à jour le token de connexion
     path('ViewUser/<int:pkUser>/', views.ViewUser), # Afficher les informations d'utilisateur
     path('LaunchMeeting/', views.LaunchMeeting), # Lancer un meeting
     path('SettingMeeting/<int:roomName>/', views.SettingMeeting), # Paramètrer une salle de réunion
